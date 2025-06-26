52e5cc9e4400547fb3d450aa2b427e8b837177c1
raj <67539971+rajzzz@users.noreply.github.com>
2025-06-26 10:59:02 +0530

    initial

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,2 @@
--- Set leader *before* anything else
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/rajzzz/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
-require("vim-options")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- .config/nvim/lua/config/lazyvim.lua
+++
@@ -1,3 +0,0 @@
-return{
-  colorsceme = "catppuccin",
-}
